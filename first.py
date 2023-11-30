091f7a59192365ce6afb5b47ef5fa23bea181bab
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-11-30 13:21:27 +0300

    project for lernin PyQT

---
+++ first.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QPushButton, QWidget, QVBoxLayout, QGridLayout, QFormLayout, QLineEdit
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("QFormLayout")
+
+layout = QFormLayout()
+layout.addRow("Name:", QLineEdit())
+layout.addRow("Age:", QLineEdit())
+layout.addRow("Job:", QLineEdit())
+layout.addRow("Hobbies:", QLineEdit())
+window.setLayout(layout)
+
+
+window.show()
+sys.exit(app.exec())
